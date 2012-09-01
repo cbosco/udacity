c0258ac55bb8989b0d1feecd0364eb8111100291
Chris Bosco <chris@aviary.com>
2012-09-01 15:25:19 -0400

    Finish solver

--- CS258_Software_Testing/3_Random_testing/sudoku_solver.py
+++ CS258_Software_Testing/3_Random_testing/sudoku_solver.py
@@ -65,22 +65,44 @@ easy = [[2,9,0,0,0,0,0,7,0],
         [0,0,1,2,0,0,3,0,6],
         [0,3,0,0,0,0,0,5,9]]
 
-# TODO
+
+# This is just a backtrack of valid,
+# above
+very_easy = [[0,0,0,0,0,0,0,0,2],
+			 [0,0,0,1,9,5,3,4,0],
+			 [0,0,0,3,4,0,0,0,7],
+			 [8,5,0,0,0,0,4,0,0],
+			 [0,2,6,8,5,3,7,9,1],
+			 [7,1,3,9,2,4,8,5,6],
+			 [0,0,0,0,0,0,0,0,0],
+			 [2,8,0,4,1,9,0,0,0],
+			 [3,0,0,2,8,6,0,0,0]]
+
+blank =	[[0,0,0,0,0,0,0,0,0],
+		 [0,0,0,0,0,0,0,0,0],
+		 [0,0,0,0,0,0,0,0,0],
+		 [0,0,0,0,0,0,0,0,0],
+		 [0,0,0,0,0,0,0,0,0],
+		 [0,0,0,0,0,0,0,0,0],
+		 [0,0,0,0,0,0,0,0,0],
+		 [0,0,0,0,0,0,0,0,0],
+		 [0,0,0,0,0,0,0,0,0]]
+
 impossible = []
 # Note: this may timeout 
 # in the Udacity IDE! Try running 
 # it locally if you'd like to test 
 # your solution with it.
 # 
-# hard = [[1,0,0,0,0,7,0,9,0],
-#         [0,3,0,0,2,0,0,0,8],
-#         [0,0,9,6,0,0,5,0,0],
-#         [0,0,5,3,0,0,9,0,0],
-#         [0,1,0,0,8,0,0,0,2],
-#         [6,0,0,0,0,4,0,0,0],
-#         [3,0,0,0,0,0,0,1,0],
-#         [0,4,0,0,0,0,0,0,7],
-#         [0,0,7,0,0,0,3,0,0]]
+hard = [[1,0,0,0,0,7,0,9,0],
+        [0,3,0,0,2,0,0,0,8],
+        [0,0,9,6,0,0,5,0,0],
+        [0,0,5,3,0,0,9,0,0],
+        [0,1,0,0,8,0,0,0,2],
+        [6,0,0,0,0,4,0,0,0],
+        [3,0,0,0,0,0,0,1,0],
+        [0,4,0,0,0,0,0,0,7],
+        [0,0,7,0,0,0,3,0,0]]
 
 def make_rep():
 	return {
@@ -109,8 +131,8 @@ def get_column(grid, column_index):
 # col 1..3
 def get_box(grid, row, col):
 	box = []
-	for row_index in range(row * 3, (row * 3)+2):
-		for col_index in range(col * 3, (col * 3)+2):
+	for row_index in range(row * 3, (row * 3)+3):
+		for col_index in range(col * 3, (col * 3)+3):
 			box.append(grid[row_index][col_index])
 	
 	return box
@@ -170,94 +192,151 @@ def check_sudoku(grid):
 	is_valid = True
 	return is_valid
 
+# Sets rep with possible guesses, and true
+# If there is only one guess, returns that guess
+# If no possible guesses, returns false
+def set_valid_guesses_for_blank (grid, blank):
+	rep = blank["guesses"] = make_rep()
+	has_possible_guess = False
+	the_one_possible_guess = 0
+	for num in get_row(grid, blank["row"]):
+		if num == 0:
+			continue
+		rep[num] = True
+	for num in get_column(grid, blank["column"]):
+		if num == 0:
+			continue
+		rep[num] = True
+
+	for num in get_box(grid, blank["row"] / 3, blank["column"] / 3):
+		if num == 0:
+			continue
+		rep[num] = True
+
+	for guess in rep:
+		if (rep[guess] == False):
+			if has_possible_guess:
+				# at least 2 possibilities
+				the_one_possible_guess = 0
+				break
+			else:
+				the_one_possible_guess = guess
+			has_possible_guess = True
+
+	if the_one_possible_guess != 0:
+		return the_one_possible_guess
+	elif has_possible_guess:
+		return True
+	else:
+		return False
+
+
 def get_blanks (grid):
 	blanks = []
-	for x in range(9):
-		for y in range(9):
-			if grid[x][y] == 0:
-				blanks.append({
-							"x": x,
-							"y": y,
+	for row in range(9):
+		for col in range(9):
+			if grid[row][col] == 0:
+				blank = {
+							"row": row,
+							"column": col,
 							"guesses": make_rep(),
 							"guessed": make_rep() 
-						})
+						}
+
+				guess = set_valid_guesses_for_blank( grid, blank )
+
+				if guess == True:
+					blanks.append( blank )
+				elif guess == False:
+					# this is a problem...
+					print "should not happen.. invalid"
+					grid[row][col] = "X"
+					print print_grid(grid)
+				else:
+					# only one solution so fill it in
+					grid[row][col] = guess
 	return blanks
 
 def set_valid_guesses (blanks, grid, offset_index = 0):
 	length = len(blanks)
 	index = offset_index
-	print "start at index " + str(index)
+	#print "set valid guesses from index " + str(index)
 	while (index < length):
 		blank = blanks[index]
-		rep = make_rep()
-		has_possible_guess = False
-		for num in get_row(grid, blank["x"]):
-			if num == 0:
-				continue
-			rep[num] = True
-		for num in get_column(grid, blank["y"]):
-			if num == 0:
-				continue
-			rep[num] = True
-		for num in get_box(grid, blank["x"] / 3, blank["x"] / 3):
-			if num == 0:
-				continue
-			rep[num] = True
-		# make sure at least one is open (False)
-		for guess in rep:
-			if (rep[guess] == False):
-				has_possible_guess = True
-				break
-		if has_possible_guess:
-			blank["guesses"] = rep 
-			index += 1
+		guess = set_valid_guesses_for_blank( grid, blank )
+		if guess == False:
+			#print "bail at index " + str(index)
+			return index
 		else:
-			print "bail at index " + str(index)
-			return False
+			index += 1
+
 
 	# valid guesses for all blanks
-	print "all valid guesses, index hit " + str(index)
 	return True
 
 
-def fill_blanks (blanks, grid):
+def fill_blanks (grid, blanks):
+	tries = 0	# debug/metrics
 	length = len(blanks)
 	index = 0
 	while (index < length):
+		tries += 1
 		blank = blanks[index]
 
 		did_fill = False
-		#print str(blank["x"]) + "," + str(blank["y"])
+
+		#print print_guesses_and_guessed( blank )
 		for guess in blank["guesses"]:
-			#print "idx: " + str(index) + ", " + str(guess) + ":" + str(blank["guesses"][guess])
 			if (blank["guesses"][guess] == False and not blank["guessed"][guess]):
-				grid[blank["x"]][blank["y"]] = guess
+				grid[blank["row"]][blank["column"]] = guess
 				blank["guessed"][guess] = True
 				did_fill = True
-				print "set blank " + str(index) + " to " + str(guess)
+				#print "[" + str(blank["row"]) + "," + str(blank["column"]) + "] - set to " + str(guess)
 				break
 
 		if (index == 0 and did_fill != True):
 			# nothing will work
-			print "fail"
+			print "failed at " + str(tries) + " tries"
 			return False
-		if (did_fill == True and set_valid_guesses(blanks, grid, index+1)):
-			index += 1
-		else:
-			print "backtrack from " + str(index)
-			last_blank = blanks[index-1]
-			# set to zero and backtrack
-			grid[blank["x"]][blank["y"]] = "X"
-			print print_grid(grid)
-			grid[blank["x"]][blank["y"]] = 0
-			blank["last_blank"] = 0
-			grid[last_blank["x"]][last_blank["y"]] = 0
-			index -= 1
-			set_valid_guesses(blanks, grid, index)
 
-	return check_sudoku(grid)
+		if (did_fill == True):
+			valid_or_bad_index = set_valid_guesses(blanks, grid, index + 1)
+			if (valid_or_bad_index == True):
+				index += 1
+				continue
 
+			elif (valid_or_bad_index > index):
+				#print "ok, bailed at future index"
+				# index stays the same, try this one again
+				blanks[valid_or_bad_index]["guessed"] = make_rep()
+				continue
 
+		#print "backtrack from " + str(index)
+		last_blank = blanks[index-1]
+		# print/debug with printed grid and an X
+		#grid[blank["row"]][blank["column"]] = "X"
+		#print print_grid(grid)
+		# reset to zero and backtrack
+		grid[blank["row"]][blank["column"]] = 0
+		# reset guesses for this blank
+		blank["guessed"] = make_rep()
+		index -= 1
+
+	return tries
+
+# debug print method
+def print_guesses_and_guessed (blank):
+	guesses_str = ""
+	for guess in blank["guesses"]:
+		if (blank["guesses"][guess] == False):
+			guesses_str += str(guess) + ","
+	guessed_str = ""
+	for guess in blank["guessed"]:
+		if (blank["guessed"][guess] == True):
+			guessed_str += str(guess) + ","
+	return "[" + str(blank["row"]) + "," + str(blank["column"]) + "] - guessed: [" + guessed_str + "] / [" + guesses_str + "]"
+
+# debug print method
 def print_grid (grid):
 	grid_str = "\n"
 	for x in range(9):
@@ -274,18 +353,224 @@ def print_grid (grid):
 
 def solve_sudoku (grid):
     ###Your code here.
+	tries = 0
 	is_valid = check_sudoku(grid)
 	if (is_valid == True):
 		blanks = get_blanks(grid)
-		if (set_valid_guesses(blanks, grid)):
-			fill_blanks(blanks, grid)
-		return print_grid(grid)
+		if (set_valid_guesses(blanks, grid) == True):
+			tries = fill_blanks(grid, blanks)
+		return "(" + str(tries) + " tries)\n" + print_grid(grid)
 		
 	else:
 		return is_valid
 
+# row test: one possibility
+assert set_valid_guesses_for_blank(
+		[[0,1,2,3,4,5,6,8,9],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0]],
+		{
+			"row": 0,
+			"column": 0,
+			"guesses": make_rep(),
+			"guessed": make_rep() 
+		}) == 7
+# row test: no possibilities
+assert set_valid_guesses_for_blank(
+		[[0,1,2,3,4,5,6,8,9],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [7,0,0,0,0,0,0,0,0]],
+		{
+			"row": 0,
+			"column": 0,
+			"guesses": make_rep(),
+			"guessed": make_rep() 
+		}) == False
+# row test: multiple possibilities
+row_test_blank = {
+			"row": 0,
+			"column": 0,
+			"guesses": make_rep(),
+			"guessed": make_rep() 
+		}
+assert set_valid_guesses_for_blank(
+		[[0,1,2,3,0,5,6,8,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0]], 
+		row_test_blank) == True
+assert row_test_blank["guesses"] == {
+			1: True,
+			2: True,
+			3: True,
+			4: False,
+			5: True,
+			6: True,
+			7: False,
+			8: True,
+			9: False
+		}
+
+# column test: one possibility
+assert set_valid_guesses_for_blank(
+        [[0,0,0,0,0,0,0,0,0],
+         [1,0,0,0,0,0,0,0,0],
+         [2,0,0,0,0,0,0,0,0],
+         [3,0,0,0,0,0,0,0,0],
+         [4,0,0,0,0,0,0,0,0],
+         [5,0,0,0,0,0,0,0,0],
+         [6,0,0,0,0,0,0,0,0],
+         [8,0,0,0,0,0,0,0,0],
+         [9,0,0,0,0,0,0,0,0]],
+		{
+			"row": 0,
+			"column": 0,
+			"guesses": make_rep(),
+			"guessed": make_rep() 
+		}) == 7
+# column test: no possibilities
+assert set_valid_guesses_for_blank(
+        [[0,0,0,0,0,0,0,0,7],
+         [1,0,0,0,0,0,0,0,0],
+         [2,0,0,0,0,0,0,0,0],
+         [3,0,0,0,0,0,0,0,0],
+         [4,0,0,0,0,0,0,0,0],
+         [5,0,0,0,0,0,0,0,0],
+         [6,0,0,0,0,0,0,0,0],
+         [8,0,0,0,0,0,0,0,0],
+         [9,0,0,0,0,0,0,0,0]],
+		{
+			"row": 0,
+			"column": 0,
+			"guesses": make_rep(),
+			"guessed": make_rep() 
+		}) == False
+# row test: multiple possibilities
+column_test_blank = {
+			"row": 0,
+			"column": 0,
+			"guesses": make_rep(),
+			"guessed": make_rep() 
+		}
+assert set_valid_guesses_for_blank(
+        [[0,0,0,0,0,0,0,0,0],
+         [1,0,0,0,0,0,0,0,0],
+         [2,0,0,0,0,0,0,0,0],
+         [3,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [5,0,0,0,0,0,0,0,0],
+         [6,0,0,0,0,0,0,0,0],
+         [8,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0]],
+		column_test_blank) == True
+assert column_test_blank["guesses"] == {
+			1: True,
+			2: True,
+			3: True,
+			4: False,
+			5: True,
+			6: True,
+			7: False,
+			8: True,
+			9: False
+		}
+
+# box test: one possibility
+assert set_valid_guesses_for_blank(
+        [[0,0,0,0,0,0,0,1,2],
+         [0,0,0,0,0,0,3,4,5],
+         [0,0,0,0,0,0,6,8,9],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0]],
+		{
+			"row": 0,
+			"column": 6,
+			"guesses": make_rep(),
+			"guessed": make_rep() 
+		}) == 7
+# box test: no possibilities
+assert set_valid_guesses_for_blank(
+        [[0,0,0,0,0,0,0,1,2],
+         [0,0,0,0,0,0,3,4,5],
+         [0,0,0,0,0,0,6,8,9],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,7,0,0]],
+		{
+			"row": 0,
+			"column": 6,
+			"guesses": make_rep(),
+			"guessed": make_rep() 
+		}) == False
+# row test: multiple possibilities
+box_test_blank = {
+			"row": 0,
+			"column": 6,
+			"guesses": make_rep(),
+			"guessed": make_rep() 
+		}
+assert set_valid_guesses_for_blank(
+        [[0,0,0,0,0,0,0,1,2],
+         [0,0,0,0,0,0,3,0,5],
+         [0,0,0,0,0,0,6,8,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0]],
+		box_test_blank) == True
+assert box_test_blank["guesses"] == {
+			1: True,
+			2: True,
+			3: True,
+			4: False,
+			5: True,
+			6: True,
+			7: False,
+			8: True,
+			9: False
+		}
+
+# run solver
+
+print "\n"
 print "ill_formed: " + str(solve_sudoku(ill_formed)) # --> None
-print "valid: " + str(solve_sudoku(valid))      # --> True
+print "\n"
+print "valid: " + str(solve_sudoku(valid))      # --> grid
+print "\n"
 print "invalid: " + str(solve_sudoku(invalid))    # --> False
-print "easy: " + str(solve_sudoku(easy))       # --> True
-#print solve_sudoku(hard)       # --> True
+print "\n"
+print "very_easy: " + str(solve_sudoku(very_easy))       # --> grid
+print "\n"
+print "blank: " + str(solve_sudoku(blank))       # --> grid
+print "\n"
+print "easy: " + str(solve_sudoku(easy))       # --> grid
+print "\n"
+print "hard:" + str(solve_sudoku(hard))       # --> grid
+print "\n"
+print "impossible:" + str(solve_sudoku(impossible))       # --> grid?
